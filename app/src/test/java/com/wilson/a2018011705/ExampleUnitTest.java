8ed785c56afaca1bddc24ddda6a5d52519be5cdc
C1 <c1@gjun.com>
2018-01-17 09:51:18 +0800

    2

--- app/src/test/java/com/wilson/a2018011705/ExampleUnitTest.java
+++ app/src/test/java/com/wilson/a2018011705/ExampleUnitTest.java
@@ -14,4 +14,13 @@ public class ExampleUnitTest {
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
     }
+    @Test
+    public void test1() throws Exception {
+        assertEquals(6, 2 + 4);
+    }
+    @Test
+    public void test2() throws Exception {
+        MyTest1 t = new MyTest1();
+        assertEquals(8, t.getAdd(2,8));
+    }
 }
